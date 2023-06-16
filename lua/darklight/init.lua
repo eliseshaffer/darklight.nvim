399a29237f321cee1d9099ea4ea7cee78ae3c886
Elise Shaffer <elise.shaffer@hey.com>
2023-06-16 08:27:16 -0600

    Extract color switch functions into strategies

--- lua/darklight/init.lua
+++ lua/darklight/init.lua
@@ -1,47 +1,12 @@
 local M = {}
 local Config = require('darklight.config')
-
-local function switch_colorscheme_if_needed(current_background)
-  local current_config = Config.current()
-  if current_config.mode ~= 'colorscheme' then
-    return false
-  end
-
-  if current_background == 'dark' then
-    vim.cmd({ cmd = 'colorscheme', args = { current_config.light_mode_colorscheme } })
-  else
-    vim.cmd({ cmd = 'colorscheme', args = { current_config.dark_mode_colorscheme } })
-  end
-end
-
-local function toggle_background(current_background)
-  if current_background == 'dark' then
-    vim.go.background = 'light'
-  else
-    vim.go.background = 'dark'
-  end
-end
-
-local function run_callbacks_if_needed(current_background)
-  local current_config = Config.current()
-
-  if current_config.mode ~= 'custom' then
-    return false
-  end
-
-  if current_background == 'dark' then
-    current_config.light_mode_callback()
-  else
-    current_config.dark_mode_callback()
-  end
-end
+local strategies = require('darklight.strategies')
 
 M.color_switch = function()
-  local current_background = vim.go.background
+  local current_config = Config.current()
 
-  switch_colorscheme_if_needed(current_background)
-  run_callbacks_if_needed(current_background)
-  toggle_background(current_background)
+  local strategy = strategies.strategy_for(current_config)
+  strategy.switch(current_config)
 end
 
 M.setup = function(config)
